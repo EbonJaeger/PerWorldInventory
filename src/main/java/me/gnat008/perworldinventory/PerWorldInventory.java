0286f39afba4fa174574ebc04940756b44a2961d
Evan <maddock.evan@gmail.com>
2014-12-29 14:43:30 -0500

    Add initial code for plugin and the Printer class

--- src/main/java/me/gnat008/perworldinventory/PerWorldInventory.java
+++ src/main/java/me/gnat008/perworldinventory/PerWorldInventory.java
@@ -1,16 +1,41 @@
+/*
+ * Copyright (C) 2014-2015  Gnat008
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package me.gnat008.perworldinventory;
 
+import me.gnat008.perworldinventory.util.Printer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PerWorldInventory extends JavaPlugin {
 
+    private static Printer printer;
+
     @Override
     public void onEnable() {
-        
+        printer = Printer.getInstance(this);
     }
 
     @Override
     public void onDisable() {
+        printer.disablePrinter();
+        printer = null;
+    }
 
+    public static Printer getPrinter() {
+        return printer;
     }
 }
--- src/main/java/me/gnat008/perworldinventory/util/Printer.java
+++ src/main/java/me/gnat008/perworldinventory/util/Printer.java
@@ -1,4 +1,80 @@
+/*
+ * Copyright (C) 2014-2015  Gnat008
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package me.gnat008.perworldinventory.util;
 
+import me.gnat008.perworldinventory.PerWorldInventory;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
 public class Printer {
+
+    private PerWorldInventory plugin;
+
+    private static Printer instance;
+
+    private Printer(PerWorldInventory plugin) {
+        this.plugin = plugin;
+    }
+
+    public static Printer getInstance(PerWorldInventory plugin) {
+        if (instance == null) {
+            instance = new Printer(plugin);
+        }
+
+        return instance;
+    }
+
+    public void disablePrinter() {
+        instance = null;
+    }
+
+    /**
+     * Method to print a message to the server console.
+     *
+     * @param msg  The message to send to console.
+     * @param warn The level of the message.
+     */
+    public void printToConsole(String msg, boolean warn) {
+        if (warn) {
+            plugin.getLogger().warning("[BlockPlaceLimiter] " + msg);
+        } else {
+            plugin.getLogger().info("[BlockPlaceLimiter] " + msg);
+        }
+    }
+
+    /**
+     * Method to print a message to a specific player on the server.
+     *
+     * @param player The player to send the message to.
+     * @param msg    The message to send.
+     * @param warn   If the message should be a warning (true for red, false for green).
+     */
+    public void printToPlayer(Player player, String msg, boolean warn) {
+        String message = "";
+
+        if (warn) {
+            message += ChatColor.RED;
+        } else {
+            message += ChatColor.GREEN;
+        }
+
+        message += "[BlockPlaceLimiter] " + msg;
+
+        player.sendMessage(message);
+    }
 }
