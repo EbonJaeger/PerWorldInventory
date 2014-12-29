a25f3fd4bd3396e98df5485ec470ea5d4da5915c
Evan <maddock.evan@gmail.com>
2014-12-29 14:38:51 -0500

    Initial commit

---
+++ src/main/java/me/gnat008/perworldinventory/PerWorldInventory.java
@@ -0,0 +1,16 @@
+package me.gnat008.perworldinventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PerWorldInventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/me/gnat008/perworldinventory/util/Printer.java
@@ -0,0 +1,4 @@
+package me.gnat008.perworldinventory.util;
+
+public class Printer {
+}
