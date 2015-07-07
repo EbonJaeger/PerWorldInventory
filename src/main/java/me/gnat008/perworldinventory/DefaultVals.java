c2e69b6c436a1e5e8a20e528817081b8f26f3a3f
ExileDev <ExileDev@hotmail.com>
2015-07-07 14:45:26 -0700

    Updated copyright information + Added license

--- src/main/java/me/gnat008/perworldinventory/DefaultVals.java
+++ src/main/java/me/gnat008/perworldinventory/DefaultVals.java
@@ -1,6 +1,6 @@
 /**
- * NexusInventory is a multi-world inventory plugin.
- * Copyright (C) 2014 - 2015Gnat008
+ * PerWorldInventory is a multi-world inventory plugin.
+ * Copyright (C) 2014 - 2015 Gnat008
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/main/java/me/gnat008/perworldinventory/PerWorldInventory.java
+++ src/main/java/me/gnat008/perworldinventory/PerWorldInventory.java
@@ -1,18 +1,19 @@
-/*
- * Copyright (C) 2014-2015  Gnat008
+/**
+ * PerWorldInventory is a multi-world inventory plugin.
+ * Copyright (C) 2014 - 2015 Gnat008
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package me.gnat008.perworldinventory;
@@ -104,10 +105,10 @@ public class PerWorldInventory extends JavaPlugin {
         getUpdater().checkUpdates();
         if (SpigotUpdater.updateAvailable()) {
             Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GRAY + "---------------------------------");
-            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "           NexusInventory Updater");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "           PerWorldInventory Updater");
             Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + " ");
-            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "An update for NexusInventory has been found!");
-            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "NexusInventory " + SpigotUpdater.getHighest());
+            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "An update for PerWorldInventory has been found!");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "PerWorldInventory " + SpigotUpdater.getHighest());
             Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "You are running " + getDescription().getVersion());
             Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + " ");
             Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Download at:");
