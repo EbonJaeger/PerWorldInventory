55b5600f1f279a13f6a8d84af4f635b3356554b1
Gnat008 <maddock.evan@gmail.com>
2016-06-26 00:11:48 -0400

    Add debug message for managing gamemode

--- src/main/java/me/gnat008/perworldinventory/listeners/player/PlayerChangedWorldListener.java
+++ src/main/java/me/gnat008/perworldinventory/listeners/player/PlayerChangedWorldListener.java
@@ -82,6 +82,9 @@ public class PlayerChangedWorldListener implements Listener {
                 playerManager.getPlayerData(groupTo, player.getGameMode(), player);
 
                 if (Settings.getBoolean("manage-gamemodes")) {
+                    if (Settings.getBoolean("debug-mode"))
+                        PerWorldInventory.printDebug("PWI manages gamemodes! Setting player '" + player.getName() + "' to gamemode " + groupTo.getGameMode().name());
+
                     player.setGameMode(groupTo.getGameMode());
                 }
             } else {
