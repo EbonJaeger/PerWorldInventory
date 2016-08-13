866223a2092aeae622259783611ef4ee0a994550
ljacqu <ljacqu@users.noreply.github.com>
2016-08-13 08:53:53 +0200

    Cleanup - create TestHelper methods for setting and getting fields (cherry picked from commit d00523d)

--- src/test/java/me/gnat008/perworldinventory/TestHelper.java
+++ src/test/java/me/gnat008/perworldinventory/TestHelper.java
@@ -1,6 +1,7 @@
 package me.gnat008.perworldinventory;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -52,6 +53,47 @@ public final class TestHelper {
             return new URI(url.toString());
         } catch (URISyntaxException e) {
             throw new IllegalStateException("File '" + path + "' cannot be converted to a URI");
+        }
+    }
+
+    /**
+     * Gets a field's value.
+     *
+     * @param clazz the class on which the field is declared
+     * @param fieldName the field name
+     * @param instance the instance to get it from (null for static fields)
+     * @param <V> the value's type
+     * @param <T> the instance's type
+     * @return the field value
+     */
+    @SuppressWarnings("unchecked")
+    public static <V, T> V getField(Class<? super T> clazz, String fieldName, T instance) {
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            // Avoid forcing user to cast
+            return (V) field.get(instance);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalStateException("Could not get field '" + fieldName + "' from " + instance, e);
+        }
+    }
+
+    /**
+     * Sets the field of the given class.
+     *
+     * @param clazz the class on which the field is declared
+     * @param fieldName the field name
+     * @param instance the instance to set the field on (null for static fields)
+     * @param value the value to set
+     * @param <T> the instance's type
+     */
+    public static <T> void setField(Class<? super T> clazz, String fieldName, T instance, Object value) {
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(instance, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalStateException("Could not set field '" + fieldName + "' on " + instance, e);
         }
     }
 }
